b3fe87408fe0fd0ae2480e38c3ab78a2fa1bcf10
yozhikvtumane <yozhikvtumane@users.noreply.github.com>
2019-06-03 11:52:56 +0100

    Updates

---
+++ precache-manifest.9dd84768330d43d09812d1c28383de05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4a0a38dc6f1775cd5709fb4d042aab",
+    "url": "/random-quote-react/index.html"
+  },
+  {
+    "revision": "477e9735e5f77ee25ad4",
+    "url": "/random-quote-react/static/css/main.19cb5e19.chunk.css"
+  },
+  {
+    "revision": "672d1fcac7d4e1d95f50",
+    "url": "/random-quote-react/static/js/2.d3ba7428.chunk.js"
+  },
+  {
+    "revision": "477e9735e5f77ee25ad4",
+    "url": "/random-quote-react/static/js/main.71329f1c.chunk.js"
+  },
+  {
+    "revision": "6fe7453b2ddf7615832b",
+    "url": "/random-quote-react/static/js/runtime~main.fa05e5a7.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import QuoteBox from './QuoteBox';
-
-ReactDOM.render(<QuoteBox />, document.getElementById('quote-box'));
-
